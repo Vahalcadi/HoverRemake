b47ba2af66e0a6558ff4948e31575e5360f061f3
Davide Petrone <davidepetrone04@gmail.com>
2024-01-17 11:12:45 +0100

    Player movement needs to be done from scratch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,21 @@
 public class Player : EntityHover
 {
+    public bool isShielded;
+    public bool isSpedUp;
+    public int invisibilityUses;
+    public int jumpUses;
+    public bool isSlowedDown;
+    public int wallUses;
+
+    public static Player Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(Instance.gameObject);
+        else
+            Instance = this;
+    }
 
 }
