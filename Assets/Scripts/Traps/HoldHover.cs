7d9160cd58eaf4066ffc9ed908078fbf3c0229a8
Vahalcadi <davidepetrone04@gmail.com>
2024-01-25 18:03:26 +0100

    adjusted HolfTrap behavior

--- Assets/Scripts/Traps/HoldHover.cs
+++ Assets/Scripts/Traps/HoldHover.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HoldHover : MonoBehaviour
 {
@@ -9,27 +11,51 @@ public class HoldHover : MonoBehaviour
     [SerializeField] private GameObject enemyBumper;
     [SerializeField] private GameObject player;
 
+    public float cooldown;
+    [NonSerialized] public float cooldownTimer;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        cooldownTimer -= Time.deltaTime;
+    }
 
+    public bool CanActivateTrap(Collider other)
+    {
+        if (cooldownTimer < 0)
+        {
+            ActivateTrap(other);
+            cooldownTimer = cooldown;
+            return true;
+        }
+
+        return false;
+    }
+
+    public virtual void ActivateTrap(Collider other)
+    {
+        StartCoroutine(TriggerTrap(other));
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        StartCoroutine(TriggerTrap(other));
+        if(cooldownTimer < 0)
+            CanActivateTrap(other);
     }
     private IEnumerator TriggerTrap(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+
+        if (other.gameObject.CompareTag("Player") && !player.GetComponent<Player>().isShielded)
         {
+
             InputManager.Instance.OnDisable();
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            player.transform.position = gameObject.transform.position;
             yield return new WaitForSeconds(trapDuration);
 
             InputManager.Instance.OnEnable();
@@ -42,10 +68,17 @@ public class HoldHover : MonoBehaviour
         }
         else if (other.gameObject.CompareTag("EnemyFlagChaser"))
         {
+            
+            enemyFlagChaser.GetComponent<NavMeshAgent>().enabled = false;
+            enemyFlagChaser.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            enemyFlagChaser.transform.position = gameObject.transform.position;
+
+            yield return new WaitForSeconds(trapDuration);
+
+            enemyFlagChaser.GetComponent<NavMeshAgent>().enabled = true;
 
         }
 
     }
-
-
 }
