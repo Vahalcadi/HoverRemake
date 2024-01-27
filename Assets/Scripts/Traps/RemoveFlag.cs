efcaae612b46fc789f67ac9527e7fa600c7f9551
Dunedus <edoardo.gobbi93@gmail.com>
2024-01-27 14:38:20 +0100

    Added RemoveFlag Traps (1 near player 1 near enemy). They both work.

---
+++ Assets/Scripts/Traps/RemoveFlag.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class RemoveFlag : Trap
+{
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override bool CanActivateTrap(Collider other)
+    {
+        return base.CanActivateTrap(other);
+    }
+
+    protected override void ActivateTrap(Collider other)
+    {
+        base.ActivateTrap(other);
+        TriggerTrap(other);
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    private void TriggerTrap(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player") && !player.GetComponent<Player>().isShielded && player.GetComponent<Player>().pickedUpFlags > 0)
+        {
+            player.GetComponent<Player>().pickedUpFlags--;
+            Debug.Log($"Removed one flag from player\nflags: {player.GetComponent<Player>().pickedUpFlags}");
+        }
+        else Debug.Log($"No flags to remove from player\nflags: {player.GetComponent<Player>().pickedUpFlags}");
+
+        if (other.gameObject.CompareTag("EnemyFlagChaser") && enemyFlagChaser.GetComponent<NavigationFlagChaser>().flagsLeft < 3)
+        {
+            enemyFlagChaser.GetComponent<NavigationFlagChaser>().flagsLeft--;
+            Debug.Log($"Removed one flag from enemy\nflags: {enemyFlagChaser.GetComponent<NavigationFlagChaser>().flagsLeft}");
+        }
+        else Debug.Log($"No flags to remove from enemy\nflags: {enemyFlagChaser.GetComponent<NavigationFlagChaser>().flagsLeft}");
+
+    }
+}
