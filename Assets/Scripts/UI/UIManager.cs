681c2c597b581d4c3d864bb3287c812b89a4be47
Dunedus <edoardo.gobbi93@gmail.com>
2024-01-28 15:25:29 +0100

    Flag sprites now change based on number of flags picked up

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,11 +9,29 @@ public class UIManager : MonoBehaviour
     [Header("Enemy References")]
     [SerializeField] NavigationFlagChaser flagChaser;
 
+    [Header("Sprites References")]
+    [SerializeField] SpriteRenderer redFlag1;
+    [SerializeField] SpriteRenderer redFlag2;
+    [SerializeField] SpriteRenderer redFlag3;
+    [SerializeField] SpriteRenderer blueFlag1;
+    [SerializeField] SpriteRenderer blueFlag2;
+    [SerializeField] SpriteRenderer blueFlag3;
+
     [Header("Texts References")]
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI jumpText;
     [SerializeField] TextMeshProUGUI wallText;
     [SerializeField] TextMeshProUGUI invisibilityText;
+
+    private void Start()
+    {
+        redFlag1.enabled = false;
+        redFlag2.enabled = false;
+        redFlag3.enabled = false;
+        blueFlag1.enabled = false;
+        blueFlag2.enabled = false;
+        blueFlag3.enabled = false;
+    }
 
     private void Update()
     {
@@ -21,5 +39,35 @@ public class UIManager : MonoBehaviour
         wallText.text = $"{player.wallUses}";
         invisibilityText.text = $"{player.invisibilityUses}";
         scoreText.text = player.score.ToString();
+
+        switch (player.pickedUpFlags)
+        {
+            case 0:
+                break;
+            case 1:
+                blueFlag1.enabled = true;
+                break;
+            case 2:
+                blueFlag2.enabled = true;
+                break;
+            case 3:
+                blueFlag3.enabled = true;
+                break;
+        }
+
+        switch (flagChaser.flagsLeft)
+        {
+            case 3:
+                break;
+            case 2:
+                blueFlag1.enabled = true;
+                break;
+            case 1:
+                blueFlag2.enabled = true;
+                break;
+            case 0:
+                blueFlag3.enabled = true;
+                break;
+        }
     }
 }
