f9c5a21cbffe5b924b25288e907845d1fc317f91
Vahalcadi <davidepetrone04@gmail.com>
2024-01-28 15:41:18 +0100

    Added script to instantiate player flags

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -7,6 +8,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] Player player;
     [SerializeField] NavigationFlagChaser flagChaser;
     [SerializeField] TextMeshProUGUI scoreText;
+
+    [SerializeField] private GameObject playerFlag;
+    [SerializeField] private GameObject enemyFlag;
+
+    [SerializeField] private int numberOfPlayerFlags;
+    [SerializeField] private int numberOfEnemyFlags;
+
+    [SerializeField] private List<GameObject> flagSpawnpoints;
+    private List<int> extractedSpawnpoints;
+    private int random;
 
     private void Update()
     {
@@ -23,6 +34,24 @@ public class GameManager : MonoBehaviour
             Debug.Log($"You lost. Score: {player.score}");
             EditorApplication.isPaused = false;
         }
+    }
 
+    private void InstantiatePlayerFlags()
+    {
+        for(int i = 0; i < numberOfPlayerFlags; i++)
+        {
+            CheckExtractedNumber();
+            Instantiate(playerFlag, flagSpawnpoints[random].transform);
+        }
+    }
+
+    private void CheckExtractedNumber()
+    {
+        random = Random.Range(0, flagSpawnpoints.Count);
+        if (extractedSpawnpoints.Contains(random))
+        {
+            CheckExtractedNumber();
+        }
+        extractedSpawnpoints.Add(random);
     }
 }
